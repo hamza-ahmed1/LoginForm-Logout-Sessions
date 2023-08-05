359295f419403412bc37eecf18903a01231bcb5e
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-08-05 01:27:02 -0700

    Initial commit

---
+++ LoginForm+Logout+Sessions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginForm_Logout_Sessions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
